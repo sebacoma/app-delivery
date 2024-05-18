823e638bcd9a513b8f1e748ddb1e7b2bfbcec301
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2024-05-17 21:08:38 -0400

    fix: corrección de crear categoría  en las rutas

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -7,7 +7,7 @@ const {getCategories} = require('../controllers/categoryController');
 
 const router = Router();
 
-router.post('/', [
+router.post('/create-category', [
     check('name', 'the field name is required').not().isEmpty(),
     check('description', 'the field description is required').not().isEmpty(),
     check('image', 'the field image is required').not().isEmpty(),
